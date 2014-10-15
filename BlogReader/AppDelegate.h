f05468014033dc0ced653fc22a8b7078024fb393
Joe Susnick <joesusnick@gmail.com>
2014-10-14 18:02:19 -0600

    Set up empty project to use storyboard

---
+++ BlogReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlogReader
+//
+//  Created by A658308 on 10/14/14.
+//  Copyright (c) 2014 Joe Susnick Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader
+//
+//  Created by A658308 on 10/14/14.
+//  Copyright (c) 2014 Joe Susnick Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
