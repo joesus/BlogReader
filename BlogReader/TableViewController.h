c504693ccef0b373797366ac7180765d769b5dcf
Joe Susnick <joesusnick@gmail.com>
2014-10-14 18:05:12 -0600

    added TableView Controller class and connected it to storyboard

---
+++ BlogReader/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  BlogReader
+//
+//  Created by A658308 on 10/14/14.
+//  Copyright (c) 2014 Joe Susnick Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
